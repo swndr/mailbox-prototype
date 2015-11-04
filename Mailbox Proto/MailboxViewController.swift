d5e61b7082add96c4b15f14a46e3631cbdcfbf12
Sam Wander <samwander@fb.com>
2015-11-04 13:09:27 -0800

    Add swipe behavior, color changes and icon changes. Todo: translate icon with swipe.

--- Mailbox Proto/MailboxViewController.swift
+++ Mailbox Proto/MailboxViewController.swift
@@ -16,7 +16,12 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var messageListScrollView: UIScrollView!
     var leftIcon: UIImageView = UIImageView()
     var rightIcon: UIImageView = UIImageView()
-    
+
+    let grayColor = UIColor(red: 234/255, green: 235/255, blue: 236/255, alpha: 1)
+    let greenColor = UIColor(red: 103/255, green: 216/255, blue: 105/255, alpha: 1)
+    let redColor = UIColor(red: 236/255, green: 85/255, blue: 35/255, alpha: 1)
+    let yellowColor = UIColor(red: 255/255, green: 222/255, blue: 86/255, alpha: 1)
+    let brownColor = UIColor(red: 213/255, green: 163/255, blue: 118/255, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +38,7 @@ class MailboxViewController: UIViewController {
         rightIcon.image = UIImage(named: "later_icon")
         messageListBackgroundView.addSubview(rightIcon)
         
+        messageListBackgroundView.backgroundColor = grayColor
         //messageListItemView.hidden = true
     }
 
@@ -43,18 +49,74 @@ class MailboxViewController: UIViewController {
 
     @IBAction func didPanMessageListItem(sender: UIPanGestureRecognizer) {
         
-        let location = sender.locationInView(view)
+        //let location = sender.locationInView(view)
+        //var msgX = messageListItemView.frame.origin.x
         let translation = sender.translationInView(view)
-        let velocity = sender.velocityInView(view)
         
+        print("method called")
+        //let velocity = sender.velocityInView(view)
         if sender.state == UIGestureRecognizerState.Began {
             print("started")
         } else if sender.state == UIGestureRecognizerState.Changed {
-            print("changed")
+            print(messageListItemView.frame.origin.x)
+            messageListItemView.frame.origin.x = translation.x
+            
+            switch messageListItemView.frame.origin.x {
+            case -320 ... -240:
+                messageListBackgroundView.backgroundColor = brownColor
+                rightIcon.image = UIImage(named: "list_icon")
+            case -239 ... -60:
+                messageListBackgroundView.backgroundColor = yellowColor
+                rightIcon.image = UIImage(named: "later_icon")
+            case -59 ... 60:
+                 messageListBackgroundView.backgroundColor = grayColor
+            case 61 ... 239:
+                 messageListBackgroundView.backgroundColor = greenColor
+                leftIcon.image = UIImage(named: "archive_icon")
+            case 240 ... 320:
+                 messageListBackgroundView.backgroundColor = redColor
+                leftIcon.image = UIImage(named: "delete_icon")
+            default:
+                print(messageListItemView.frame.origin.x)
+            }
+            
+            
         } else if sender.state == UIGestureRecognizerState.Ended {
-            print("ended")
+            switch messageListItemView.frame.origin.x {
+            case -320 ... -240:
+                print("R2")
+                UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                    self.messageListItemView.frame.origin.x = -320
+                    }, completion: { (Bool) -> Void in
+                })
+            case -239 ... -60:
+                print("R1")
+                UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                    self.messageListItemView.frame.origin.x = -320
+                    }, completion: { (Bool) -> Void in
+                })
+            case -59 ... 60:
+                print("Bounce")
+                UIView.animateWithDuration(0.15, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                    self.messageListItemView.frame.origin.x = 0
+                    }, completion: { (Bool) -> Void in
+                })
+            case 61 ... 239:
+                print("L1")
+                UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                    self.messageListItemView.frame.origin.x = 320
+                    }, completion: { (Bool) -> Void in
+                })
+            case 240 ... 320:
+                print("L2")
+                UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                    self.messageListItemView.frame.origin.x = 320
+                    }, completion: { (Bool) -> Void in
+                })
+            default:
+                print(messageListItemView.frame.origin.x)
+            }
         }
-        
     }
 
 }
