b4fcb32df55893625ade2735d21e5d7ef427a21b
Sam Wander <samwander@fb.com>
2015-11-03 17:27:38 -0800

    Empty project with assets

--- Mailbox Proto/MailboxViewController.swift
+++ Mailbox Proto/MailboxViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MailboxViewController.swift
 //  Mailbox Proto
 //
 //  Created by Sam Wander on 11/3/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MailboxViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
