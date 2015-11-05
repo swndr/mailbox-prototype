6cec53334a50aceb419959bf07f645fb124e1918
Sam Wander <samwander@fb.com>
2015-11-05 09:01:03 -0800

    Hide message row after swiping, added the list and reschedule views which can be tapped to dismiss.

--- Mailbox Proto/MailboxViewController.swift
+++ Mailbox Proto/MailboxViewController.swift
@@ -11,20 +11,34 @@ import UIKit
 class MailboxViewController: UIViewController {
 
     @IBOutlet weak var mailboxSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var feedView: UIImageView!
     @IBOutlet weak var messageListItemView: UIImageView!
     @IBOutlet weak var messageListBackgroundView: UIView!
     @IBOutlet weak var messageListScrollView: UIScrollView!
     var leftIcon: UIImageView = UIImageView()
     var rightIcon: UIImageView = UIImageView()
+    
+    @IBOutlet weak var rescheduleView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
 
+    // Mailbox colors
     let grayColor = UIColor(red: 234/255, green: 235/255, blue: 236/255, alpha: 1)
     let greenColor = UIColor(red: 103/255, green: 216/255, blue: 105/255, alpha: 1)
     let redColor = UIColor(red: 236/255, green: 85/255, blue: 35/255, alpha: 1)
     let yellowColor = UIColor(red: 255/255, green: 222/255, blue: 86/255, alpha: 1)
     let brownColor = UIColor(red: 213/255, green: 163/255, blue: 118/255, alpha: 1)
     
+    // Dynamic sizes
+    var dw: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        rescheduleView.alpha = 0.0
+        listView.alpha = 0.0
+        
+        dw = view.frame.size.width
         messageListScrollView.contentSize = CGSize(width:320,height:1438)
         mailboxSegmentedControl.selectedSegmentIndex = 1
         
@@ -60,26 +74,32 @@ class MailboxViewController: UIViewController {
             
             // Change list item background color and action icons
             switch messageListItemView.frame.origin.x {
-            case -320 ... -240:
+            case (dw * -1) ... -240:
                 messageListBackgroundView.backgroundColor = brownColor
                 rightIcon.image = UIImage(named: "list_icon")
                 rightIcon.frame.origin.x = messageListItemView.frame.origin.x + messageListItemView.frame.size.width + 20
+                leftIcon.hidden = true
             case -239 ... -60:
                 messageListBackgroundView.backgroundColor = yellowColor
                 rightIcon.image = UIImage(named: "later_icon")
                 rightIcon.frame.origin.x = messageListItemView.frame.origin.x + messageListItemView.frame.size.width + 20
+                leftIcon.hidden = true
             case -59 ... 59:
                 messageListBackgroundView.backgroundColor = grayColor
                 leftIcon.frame.origin.x = 20
                 rightIcon.frame.origin.x = view.frame.size.width-45
+                leftIcon.hidden = false
+                rightIcon.hidden = false
             case 60 ... 239:
                 messageListBackgroundView.backgroundColor = greenColor
                 leftIcon.image = UIImage(named: "archive_icon")
                 leftIcon.frame.origin.x = messageListItemView.frame.origin.x - 40
-            case 240 ... 320:
+                rightIcon.hidden = true
+            case 240 ... dw:
                 messageListBackgroundView.backgroundColor = redColor
                 leftIcon.image = UIImage(named: "delete_icon")
                 leftIcon.frame.origin.x = messageListItemView.frame.origin.x - 40
+                rightIcon.hidden = true
             default:
                 print(messageListItemView.frame.origin.x)
             }
@@ -89,17 +109,29 @@ class MailboxViewController: UIViewController {
             
             // Decide were to snap list item
             switch messageListItemView.frame.origin.x {
-            case -320 ... -240:
+            case (dw * -1) ... -240:
                 // List
                 UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
-                    self.messageListItemView.frame.origin.x = -320
+                    self.messageListItemView.frame.origin.x = (self.dw * -1) - 40
+                    self.rightIcon.frame.origin.x = (self.dw * -1) - 80
                     }, completion: { (Bool) -> Void in
+                        self.hideMessage()
+                        UIView.animateWithDuration(0.2, delay: 0.2, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                            self.listView.alpha = 1.0
+                            }, completion: { (Bool) -> Void in
+                        })
                 })
             case -239 ... -60:
                 // Later
                 UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
-                    self.messageListItemView.frame.origin.x = -320
+                    self.messageListItemView.frame.origin.x = (self.dw * -1) - 40
+                    self.rightIcon.frame.origin.x = (self.dw * -1) - 80
                     }, completion: { (Bool) -> Void in
+                        self.hideMessage()
+                        UIView.animateWithDuration(0.2, delay: 0.2, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+                            self.rescheduleView.alpha = 1.0
+                            }, completion: { (Bool) -> Void in
+                        })
                 })
             case -59 ... 59:
                 // Return
@@ -110,19 +142,45 @@ class MailboxViewController: UIViewController {
             case 60 ... 239:
                 // Archive
                 UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
-                    self.messageListItemView.frame.origin.x = 320
+                    self.messageListItemView.frame.origin.x = self.dw + 40
+                    self.leftIcon.frame.origin.x = self.dw + 80
                     }, completion: { (Bool) -> Void in
+                        self.hideMessage()
                 })
-            case 240 ... 320:
+            case 240 ... dw:
                 // Delete
                 UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
-                    self.messageListItemView.frame.origin.x = 320
+                    self.messageListItemView.frame.origin.x = self.dw + 40
+                    self.leftIcon.frame.origin.x = self.dw + 80
                     }, completion: { (Bool) -> Void in
+                        self.hideMessage()
                 })
             default:
                 print(messageListItemView.frame.origin.x)
             }
         }
+    }
+    
+    func hideMessage() {
+        UIView.animateWithDuration(0.2, delay: 0.0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+            self.feedView.frame.origin.y = 142
+            }, completion: { (Bool) -> Void in
+        })
+    }
+    
+    @IBAction func didTapListView(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.2, delay: 0.2, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+            self.listView.alpha = 0.0
+            }, completion: { (Bool) -> Void in
+        })
+    }
+    
+    
+    @IBAction func didTapRescheduleView(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.2, delay: 0.2, options: [UIViewAnimationOptions.CurveEaseInOut], animations: { () -> Void in
+            self.rescheduleView.alpha = 0.0
+            }, completion: { (Bool) -> Void in
+        })
     }
 
 }
